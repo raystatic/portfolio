33db719ef589c6c851e80d7ac97ae6a35020ec88
raystatic <rahul9650ray@gmail.com>
2021-05-04 16:00:09 +0530

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-
-<!-- ![ReactJS Resume Website Template](resume-screenshot.png?raw=true "ReactJS Resume Website Template") -->
---
+++ precache-manifest.b9e6977661138e507b0eb781faf1a577.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09fe016fd5518b93ce6ea0860c15efa8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c537b0dda641a0d6b0b8",
+    "url": "/portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "946f1715194b8a7a59e8",
+    "url": "/portfolio/static/js/2.d3e4eafd.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/portfolio/static/js/2.d3e4eafd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c537b0dda641a0d6b0b8",
+    "url": "/portfolio/static/js/main.cc350356.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
