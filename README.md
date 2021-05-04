df4250c1d59ad8f6ca66150086e53ab2102992eb
raystatic <rahul9650ray@gmail.com>
2021-05-04 15:38:31 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+
+![ReactJS Resume Website Template](resume-screenshot.png?raw=true "ReactJS Resume Website Template")
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+window.location.replace("https://keen-visvesvaraya-dd8774.netlify.app/");
+
